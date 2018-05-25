66199d1e4d82de8e7e4c0e6e3869be6cbb487eb3
Matthew <mattsjsu14@gmail.com>
2018-05-25 16:25:54 -0700

    initial file setup

---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+
+
+
+
+
+// Export the database functions for the controller (burger_Controller.js).
+module.exports = burger;
